cad5e2a337f94a07dce938c003162d663623a25f
kosa <kosa@kosa.com>
2025-06-12 09:25:35 +0900

    수정

--- src/main/java/org/kosa/hello/HomeController.java
+++ src/main/java/org/kosa/hello/HomeController.java
@@ -13,6 +13,22 @@ public class HomeController {
 	@GetMapping("/")
 	public String index(ServletRequest request) {
 		log.info("local port = {} : {}", request.getLocalPort(), request.getServerPort());
-		return "Hello World... " + request.getLocalPort() + " : " + request.getServerPort() ;
+		return "변경 Hello World... " + request.getLocalPort() + " : " + request.getServerPort() ;
 	}
 }
+
+/*
+ * 1. 개발자 pc에서 진행하는 절차 
+ *    1. git commit
+ *    2. git push 
+ * 2. 운영에서 진행하는 절차 
+ *    1. git pull
+ *    2. ./gradlew bootJar
+ *    3. docker build -t 계정명/이미지명:latest . 
+ *    4. docker push 계정명/이미지명:latest
+ *    5. docker-compose down 
+ *    6. docker-compose pull(생략 가능)
+ *    7. docker-compose up -d 
+ *    
+ */
+
